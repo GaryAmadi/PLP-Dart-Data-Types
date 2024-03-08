27ff12345a52d3277d308fc8461a8b8117f9b6e0
Gary Amadi <135960511+GaryAmadi@users.noreply.github.com>
2024-03-08 04:41:52 +0300

    Add files via upload

---
+++ datatypesassignment.dart
@@ -0,0 +1,22 @@
+//Data Types in Dart
+void main() {
+  //Int to store whole numbers
+  int age = 21;
+  print("I am $age years old");
+
+  //double for storing decimals
+  double rate = 22.58;
+  print("The interest rate is $rate");
+
+  //String for storing text
+  String name = "Gary Amadi";
+  print("My name is $name");
+
+  //List for storing a collection
+  List<String> players = ["Saliba", "Rice", "Saka"];
+  print("Arsenal players: $players");
+
+  //Map for storing key pair values
+  Map<String, int> playerages = {"Saliba": 22, "Rice": 25, "Saka": 22};
+  print("Arsenal player ages: $playerages");
+}
